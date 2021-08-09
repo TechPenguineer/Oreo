9ff66abc42af619e07b9ab3efa706016062e47d9
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-09 12:52:29 -0400

    pass CrunchStringArguments

--- crunch.jl
+++ crunch.jl
@@ -1,17 +1,5 @@
-"""
-    crunch()
-
-- Julia version: 
-- Author: Dev
-- Date: 2021-08-09
-
-# Arguments
-
-# Examples
-
-```jldoctest
-julia>
-```
-"""
-function crunch()
+function crunch(CrunchString::String)
+    print(CrunchString);
 end
+
+crunch("Hello!")
